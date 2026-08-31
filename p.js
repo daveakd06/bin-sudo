a57615a3faaa61b4ca3a7c34e368d9f0764cc2e9
daveakd06 <dev-benin@epitech.africa>
2026-08-31 20:19:02 +0200

    grad

--- p.js
+++ p.js
@@ -1,9 +1,4 @@
-(function(){var b='/cases.php/C00254_';
-function chain(a,i){ if(i>=a.length) return; var im=new Image(); im.onload=im.onerror=function(){chain(a,i+1);}; im.src=a[i]; }
-var H='';try{H=document.documentElement.outerHTML;}catch(e){}
-var hku=location.href.indexOf('api_key')>=0?1:0;
-var hkh=H.indexOf('api_key')>=0?1:0;
-var hbear=H.indexOf('Bearer')>=0?1:0;
-var hb2=H.indexOf('HLB2026')>=0?1:0;
-chain([b+'C1.css', b+'HKU'+hku+'.css', b+'HKH'+hkh+'.css', b+'BEAR'+hbear+'.css', b+'FLAG'+hb2+'.css', b+'C2.css', b+'C3.css', b+'C4.css'],0);
+(function(){var b='/cases.php/G00342_';
+new Image().src=b+'T0.css';
+[30,60,100,150,200,300,400,500,700,1000].forEach(function(ms){ setTimeout(function(){new Image().src=b+'T'+ms+'.css';},ms); });
 })();

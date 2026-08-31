8be8261e10a5ca2f9bbb453584e4ccffc9d22740
daveakd06 <dev-benin@epitech.africa>
2026-08-31 20:15:54 +0200

    acc

--- p.js
+++ p.js
@@ -1,7 +1,4 @@
-(function(){var b='/cases.php/W0020_';function I(s){new Image().src=b+s+'.css';}
-function W(ms){var t=Date.now();while(Date.now()-t<ms){}}
-I('W1'); W(400);
-try{var H=document.documentElement.outerHTML; I(H.length>1500?'Hbig':'Hsml'); I(H.indexOf('/admin/')>=0?'adm1':'adm0'); }catch(e){I('Hexc');}
-W(400);
-I('W2'); W(400); I('W3');
-})();
+new Image().src='/account.php/A00154.css';
+new Image().src='/admin/index.php/A00154.css';
+new Image().src='/admin/review.php/A00154.css';
+new Image().src='/cases.php/A00154_RAN.css';

